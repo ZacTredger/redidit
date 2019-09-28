9e39972393bb46405d441228d2a82f158dbc5f4b
Zac <zac4793@gmail.com>
2019-09-28 11:38:57 +0100

    Add digest class method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# Models a site user
 class User < ApplicationRecord
   has_many :posts
   validates :username, presence: true, length: { maximum: 255 }
@@ -8,4 +9,22 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 7 }
   before_save { email.downcase! }
+
+  class << self
+    # Returns the digest of a given string
+    def digest(password)
+      BCrypt::Password.create(password, cost: hash_cost)
+    end
+
+    private
+
+    # Retrieves computational cost of hashing: 4 in test, else 10
+    def hash_cost
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    end
+  end
 end
