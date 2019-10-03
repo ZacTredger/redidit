a61e7c7a8286737f11d2e91983701c566fc94255
Zac <zac4793@gmail.com>
2019-10-03 09:24:35 +0100

    Add sessions resource, routes and link in layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :sessions, only: %i[new create destroy]
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
@@ -6,5 +8,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
-    # assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', login_path
   end
 end
