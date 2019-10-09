08ff6bb58d1a55284194be4e57d4a46246404eef
Zac <zac4793@gmail.com>
2019-10-09 17:02:37 +0100

    Enable post editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   include ApplicationHelper
   before_action :logged_in_user, except: %i[index show]
+  before_action :set_post_var, only: %i[show edit update destroy]
+  before_action :correct_user, only: %i[edit update destroy]
   def index; end
 
   def show
-    return if (@post = Post.find_by(id: params[:id]))
+    return if @post
 
     flash[:error] = 'Post not found'
     redirect_to root_path
@@ -21,9 +23,19 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
-  def edit; end
+  def edit
+    return if @post
 
-  def update; end
+    flash[:error] = 'Post not found'
+    redirect_to root_path
+  end
+
+  def update
+    return (render :edit) unless
+      @post&.update(post_params.merge(user_id: current_user.id))
+
+    redirect_to @post
+  end
 
   def destroy; end
 
@@ -31,5 +43,16 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :link, :body)
+  end
+
+  # Before-filters
+  # Sets the instance var @post, which is used to test whether user is correct
+  def set_post_var
+    @post = Post.find_by(id: params[:id])
+  end
+
+  # Tests whether the user trying to interact with the post is its author
+  def correct_user
+    super(@post.user_id)
   end
 end
