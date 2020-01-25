fce1013360efbd0a05b021eaca7a976b76f7889b
Zac <zac4793@gmail.com>
2020-01-25 12:30:23 +0000

    Display metadata above comments
    Made some tweaks to the way all metadata is displayed above post/comment
    text.
    
    Added a test to check comment text was displaying correctly. It was
    initially failing, but eventually I discovered it was because I had
    padded the erb with spaces either-side: `<p> <%= foo %> </p>`, and those
    spaces were obviously being included. (It's tempting to make this
    mistake because erb autocomplete isn't activated if `<` is typed
    contiguously with preceding spaces.)

--- test/integration/posts/posts_comments_test.rb
+++ test/integration/posts/posts_comments_test.rb
@@ -3,7 +3,19 @@ require 'test_helper'
 class PostsCommentsTest < ActionDispatch::IntegrationTest
   test 'post displays comments' do
     post = create(:post_with_comments)
+    text_to_comment = post.comments.each_with_object({}) do |comment, hsh|
+      hsh[comment.text] = comment
+    end
     get post_path(post)
-    assert_select 'div.comment', count: 5
+    assert_select 'div.comment', count: 5 do |cmnt_objs|
+      cmnt_objs.each do |cmnt_obj|
+        assert_select cmnt_obj, 'p.comment-text', count: 1 do |(comment_p)|
+          # Remove each matched comment from the list so they only match once
+          assert comment = text_to_comment.delete(comment_p.text)
+          assert_select cmnt_obj, 'a[href=?]', user_path(user = comment.user),
+                        text: /#{user.username}/
+        end
+      end
+    end
   end
 end
