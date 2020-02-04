2e79bb8aac4ec4222772e73e5f52788d59fab292
Zac <zac4793@gmail.com>
2020-02-04 13:42:47 +0000

    Add custom error pages
    This engables the retention of the layout on error pages, adding
    consistency to the user experience. I retained the basic design of
    rails' default error pages, but used brand colours.

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get "/404", to: "errors#not_found"
+  get "/422", to: "errors#unacceptable"
+  get "/500", to: "errors#internal_error"
 end
